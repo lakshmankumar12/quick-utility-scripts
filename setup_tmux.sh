4c7595754bfd3b805b1638880276c30c0fd15825
lakshman kumar <lakshmankumar@gmail.com>
2017-07-10 11:16:41 -0700

    add more windows to tmux-setup

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -9,9 +9,15 @@ if [ $1 == "-k" ] ; then
   tmux send-keys -t "nixPow" Escape ":qa" "C-m"
   tmux send-keys -t "lang" Escape ":qa" "C-m"
   tmux send-keys -t "general" Escape ":qa" "C-m"
+  tmux send-keys -t "pynotes" Escape ":qa" "C-m"
   tmux send-keys -t "dotf" Escape ":qa" "C-m"
   tmux send-keys -t "vimf" Escape ":qa" "C-m"
   tmux send-keys -t "docker" Escape ":qa" "C-m"
+  tmux send-keys -t "sshvpn" Escape ":qa" "C-m"
+  tmux send-keys -t "mio-grab" Escape ":qa" "C-m"
+  tmux send-keys -t "iQuest" Escape ":qa" "C-m"
+  tmux send-keys -t "iBit" Escape ":qa" "C-m"
+  tmux send-keys -t "vim-term" Escape ":qa" "C-m"
 
 else
 
@@ -22,9 +28,15 @@ else
   tmux new-window -d -t 2 -n "nixPow" -c "$HOME/github/nixPowerToolNotes"
   tmux new-window -d -t 3 -n "lang" -c "$HOME/github/languageNotes"
   tmux new-window -d -t 4 -n "general" -c "$HOME/github/general_reading_notes"
-  tmux new-window -d -t 5 -n "dotf" -c "$HOME/github/dotfiles"
-  tmux new-window -d -t 6 -n "vimf" -c "$HOME/github/vimfiles"
-  tmux new-window -d -t 7 -n "docker" -c "$HOME/github/devbox"
+  tmux new-window -d -t 5 -n "pynotes" -c "$HOME/github/pynotes"
+  tmux new-window -d -t 6 -n "dotf" -c "$HOME/github/dotfiles"
+  tmux new-window -d -t 7 -n "vimf" -c "$HOME/github/vimfiles"
+  tmux new-window -d -t 8 -n "docker" -c "$HOME/github/devbox"
+  tmux new-window -d -t 9 -n "sshvpn" -c "$HOME/gitlab/aryaka-vpn"
+  tmux new-window -d -t 10 -n "mio-grab" -c "$HOME/gitlab/mio-track-grab"
+  tmux new-window -d -t 11 -n "iQuest" -c "$HOME/gitlab/interviewquestions"
+  tmux new-window -d -t 12 -n "iBit" -c "$HOME/gitlab/interview-bit"
+  tmux new-window -d -t 13 -n "vim-term" -c "$HOME/gitlab/interview-bit"
 
   tmux send-keys -t "mac-notes" "vi" "C-m"
   #tmux send-keys -t "mac-notes" "Esc" ":e scratchpad" "C-m" "Esc" ":tabnew maintodo.org" "C-m"
@@ -32,9 +44,15 @@ else
   tmux send-keys -t "nixPow" "vi" "C-m"
   tmux send-keys -t "lang" "vi" "C-m"
   tmux send-keys -t "general" "vi" "C-m"
+  tmux send-keys -t "pynotes" "vi" "C-m"
   tmux send-keys -t "dotf" "vi" "C-m"
   tmux send-keys -t "vimf" "vi" "C-m"
   tmux send-keys -t "docker" "vi" "C-m"
+  tmux send-keys -t "sshvpn" "sshvpndoc" "C-m"
+  tmux send-keys -t "mio-grab" "vi" "C-m"
+  tmux send-keys -t "iQuest" "vi" "C-m"
+  tmux send-keys -t "iBit" "vi" "C-m"
+  tmux send-keys -t "vim-term" "vi" "C-m"
 
   tmux -2 attach -d -t mac-main
 fi
