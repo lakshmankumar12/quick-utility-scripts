2f40b106fcfdf492783f82c6dd118a3b3e10746d
lakshman kumar <lakshmankumar@gmail.com>
2017-11-29 11:00:05 -0800

    use git to list src-files, load-summary to print stuff only if its higher

--- load_summary.py
+++ load_summary.py
@@ -8,7 +8,15 @@ def load_summary():
     load = os.getloadavg()
     cpu_percent = psutil.cpu_percent(interval=1)
     cpu_times_percent = psutil.cpu_times_percent()
-    print ("Load: {:.2f} Total%: {:.2f} user%: {:.2f} system: {:.2f}".format(load[0],cpu_percent,cpu_times_percent.user,cpu_times_percent.system))
+    op = "L: {:.2f}".format(load[0])
+    if cpu_percent > 10.0:
+        op += " T: {:.2f}%".format(cpu_percent)
+    if cpu_times_percent.user > 10.0:
+        op += " U: {:.2f}%".format(cpu_times_percent.user)
+    if cpu_times_percent.system > 10.0:
+        op += " S: {:.2f}%".format(cpu_times_percent.system)
+    #print ("L: {:.2f} T%: {:.2f} U%: {:.2f} S%: {:.2f}".format(load[0],cpu_percent,cpu_times_percent.user,cpu_times_percent.system))
+    print (op)
 
 if __name__ == '__main__':
     load_summary()
