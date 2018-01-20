e5d07e2e51e1220205711dee863032859f7cbdbe
lakshman kumar <lakshmankumar@gmail.com>
2018-01-19 17:34:22 -0800

    lyrics with more sources

--- get_lyrics.py
+++ get_lyrics.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
-import PyLyrics
 import argparse
+import sys
+
+import lyricwikia
+import PyLyrics
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("title",   help="Song title")
@@ -9,4 +13,20 @@ parser.add_argument("author",  help="Author")
 
 parsed_args = parser.parse_args()
 
-print(PyLyrics.PyLyrics.getLyrics(parsed_args.author,parsed_args.title))
+def get_lyrics(author, title):
+    try:
+        lyrics = lyricwikia.get_lyrics(author,title)
+        return (lyrics, '(1)lyricwikia')
+    except:
+        print ("Couldn't get it off lyricwikia.. Trying pylyrics")
+
+    try:
+        lyrics = PyLyrics.PyLyrics.getLyrics(author,title)
+        return (lyrics,'(2)pylyrics')
+    except ValueError:
+        print ("Couldn't get it off pylyrics")
+        sys.exit(1)
+
+(lyrics,source) = get_lyrics(parsed_args.author,parsed_args.title)
+print ("Lyrics form {} for title: {} - author: {}".format(source,parsed_args.title,parsed_args.author))
+print (lyrics.encode('ascii','xmlcharrefreplace'))
--- pretty_json.py
+++ pretty_json.py
@@ -2,5 +2,11 @@
 
 from __future__ import print_function
 import fileinput, json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename.js",  help="input json (stdin used if skipped)", nargs="?")
+
+parsed_args = parser.parse_args()
 
 print(json.dumps(json.loads("".join(fileinput.input())), sort_keys=True, indent=4))
