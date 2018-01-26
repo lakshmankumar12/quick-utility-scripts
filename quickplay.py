6fbdb6cbf19a8ae99ee71e4472d2d46ca92187de
lakshman kumar <lakshmankumar@gmail.com>
2018-01-26 15:50:22 -0800

    Iniital cut of adding tqdm

--- quickplay.py
+++ quickplay.py
@@ -8,6 +8,7 @@ import sys
 from threading import Thread
 from time import sleep
 import select
+import tqdm
 
 user_in_loop = 1
 play_pause_state = 1
@@ -18,39 +19,45 @@ class GetUserInput(Thread):
         global play_pause_state
         global player
         prompt = "Command:"
-        print  (prompt, end=""); sys.stdout.flush()
-        while (user_in_loop):
-            try:
-                i, o, e = select.select( [sys.stdin], [], [], 10 )
-                if (i):
-                    variable=sys.stdin.readline().strip()
-                    if variable == 'q':
-                        user_in_loop = 0
-                        break
-                    elif variable == 'p':
-                        player.pause()
-                        play_pause_state = 1 - play_pause_state
-                    elif variable == 'l':
-                        print("Length:{} , Time:{}".format(player.get_length(), player.get_time()))
-                    elif variable == 'gv':
-                        print ("current volume is {}".format(player.audio_get_volume()))
-                    elif variable.startswith('v '):
-                        level = int(variable[2:])
-                        if level:
-                            print("setting volume to {}".format(level))
-                            player.audio_set_volume(level)
-                    elif variable.startswith('s '):
-                        time = int(variable[2:])
-                        if time < player.get_length() and time > 0:
-                            player.set_time(time)
+        pbar_tot = player.get_length()
+        pbar_sofar = 0
+        with tqdm.tqdm(total=pbar_tot, unit="s", unit_scale=True, unit_divisor=1000) as pbar:
+            pbar.set_description(prompt)
+            while (user_in_loop):
+                try:
+                    i, o, e = select.select( [sys.stdin], [], [], 1)
+                    pbar_new = player.get_time() - pbar_sofar
+                    if (pbar_new > 0):
+                        pbar.update(pbar_new)
+                        pbar_sofar += pbar_new
+                    if (i):
+                        variable=sys.stdin.readline().strip()
+                        if variable == 'q':
+                            user_in_loop = 0
+                            break
+                        elif variable == 'p':
+                            player.pause()
+                            play_pause_state = 1 - play_pause_state
+                        elif variable == 'l':
+                            print("Length:{} , Time:{}".format(player.get_length(), player.get_time()))
+                        elif variable == 'gv':
+                            print ("current volume is {}".format(player.audio_get_volume()))
+                        elif variable.startswith('v '):
+                            level = int(variable[2:])
+                            if level:
+                                print("setting volume to {}".format(level))
+                                player.audio_set_volume(level)
+                        elif variable.startswith('s '):
+                            time = int(variable[2:])
+                            if time < player.get_length() and time > 0:
+                                player.set_time(time)
+                            else:
+                                print ("time:{} is out of range 1-{}".format(time, player.get_length()))
                         else:
-                            print ("time:{} is out of range 1-{}".format(time, player.get_length()))
-                    else:
-                        print("didnt understand:{}".format(variable))
-                    print  (prompt, end=""); sys.stdout.flush()
-            except:
-                user_in_loop = 0
-                raise
+                            print("didnt understand:{}".format(variable))
+                except:
+                    user_in_loop = 0
+                    raise
 
 class PlayerWait(Thread):
     def run(self):
@@ -72,4 +79,3 @@ t1.start()
 t2.start()
 t2.join()
 t1.join()
-print 
