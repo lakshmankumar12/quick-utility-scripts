85084b313a5d6af2354a4f4f97d8056dc6b07da0
lakshman kumar <lakshmankumar@gmail.com>
2018-01-29 17:06:13 -0800

    get ps and terms

---
+++ get_ps_and_terms.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import psutil
+import pdb
+import collections
+import argparse
+
+process_hierarchy = {}
+root = None
+unparented = collections.defaultdict(list)
+
+class MyProcess:
+    def __init__(self, p):
+        global root
+        global process_hierarchy
+        self.p = p
+        self.name = p.name()
+        self.pid = p.pid
+        self.ppid = p.ppid()
+        self.tty = p.terminal()
+        if self.ppid == 0:
+            root = self
+            self.parent = self
+        else:
+            if self.ppid in process_hierarchy:
+                self.parent = process_hierarchy[self.ppid]
+                self.parent.children.append(self)
+            else:
+                unparented[self.ppid].append(self)
+        process_hierarchy[self.pid] = self
+        self.children = []
+        if self.pid in unparented:
+            for ip in unparented[self.pid]:
+                ip.parent = self
+                self.children.append(ip)
+            del unparented[self.pid]
+
+indentString=u"\u250a\u2027\u2027"
+def print_hierarchy(myp, indentLevel):
+    highlight_term = ""
+    if myp.parent.tty != myp.tty:
+        highlight_term = "**"
+    print (u"{}{}:{} --> {}{}".format(indentString*indentLevel, myp.pid, myp.name, highlight_term, myp.tty))
+    for i in myp.children:
+        print_hierarchy(i, indentLevel+1)
+
+def main():
+    global root
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pid",   help="pid to start", nargs="?", type=int, default=1)
+    cmd_options = parser.parse_args()
+
+    #collect all pros
+    for p in psutil.process_iter():
+        myp = MyProcess(p)
+
+    if (unparented):
+        raise(Exception("unparented not empty!"))
+
+    if cmd_options.pid != 1:
+        root = process_hierarchy[cmd_options.pid]
+
+    print_hierarchy(root, 0)
+
+try:
+    main()
+except Exception,e:
+    pdb.set_trace()
