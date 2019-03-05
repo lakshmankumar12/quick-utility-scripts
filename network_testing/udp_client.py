ce335568db0fd92c2452d92516576345b1cc6ce0
lakshman kumar <lakshmankumar@gmail.com>
2019-03-05 14:52:52 -0800

    adding udp client and renaming to echo server

--- network_testing/udp_client.py
+++ network_testing/udp_client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 # <!--licensing stuff
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
@@ -6,11 +7,8 @@
 
 from __future__ import print_function
 
-from twisted.internet import task
+from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
-from twisted.internet.defer import Deferred
-from twisted.internet.protocol import ClientFactory
-from twisted.protocols.basic import LineReceiver
 
 import argparse
 import sys
@@ -21,46 +19,32 @@ def echoClientCallback(arg):
 def batchCallBack(arg):
     arg.start_a_batch()
 
-class EchoClient(LineReceiver):
+class EchoClientDatagramProtocol(DatagramProtocol):
 
-    def __init__(self):
-        LineReceiver.__init__(self)
-        self.inited=0
-        #further inits will be done in postFactorySetInit
-
-    def postFactorySetInit(self):
-        self.myinstance=next(self.factory.counter)
-        self.inited=1
-        self.transactions_done = 0
+    def __init__(self, mgr, myinstance):
+        DatagramProtocol.__init__(self)
+        self.mgr = mgr
+        self.myinstance = myinstance
         self.complete = 0
+        self.transactions_done = 0
 
-    def connectionMade(self):
-        if not self.inited:
-            self.postFactorySetInit()
+    def startProtocol(self):
+        self.transport.connect(self.mgr.opts.serverip, self.mgr.opts.serverport)
         self.initiate_a_transaction()
 
     def initiate_a_transaction(self):
         tosend = "This is line:{} from instance:{}".format(self.transactions_done, self.myinstance)
-        self.sendLine(bytes(tosend, 'utf-8'))
+        self.transport.write(bytes(tosend, 'utf-8'))
 
-    def lineReceived(self, line):
-        print("receive:", line)
+    def datagramReceived(self, datagram, host):
+        print('Datagram received: ', repr(datagram))
         self.transactions_done += 1
-        if self.transactions_done < self.factory.opts.transactions:
-            reactor.callLater(self.factory.opts.timeout, echoClientCallback, self)
+        if self.transactions_done < self.mgr.opts.transactions:
+            reactor.callLater(self.mgr.opts.timeout, echoClientCallback, self)
         else:
             self.complete = 1
-            self.transport.loseConnection()
-
-    def connectionLost(self, reason):
-        print('connection failed for {},reason:{}'.format(self.myinstance,reason.getErrorMessage()))
-
-    def connectionLost(self, reason):
-        if not self.complete:
-            print('connection lost for {},reason:{}'.format(self.myinstance,reason.getErrorMessage()))
-        else:
             print('done for client:{}'.format(self.myinstance))
-
+            self.mgr.clientDone()
 
 def counter():
     cntr = 1
@@ -68,47 +52,32 @@ def counter():
         yield cntr
         cntr += 1
 
-
-
-class EchoClientFactory(ClientFactory):
-    protocol = EchoClient
-
+class Manager:
     def __init__(self, opts):
-        self.done = Deferred()
         self.opts = opts
         self.counter = counter()
         self.clients_created = 0
         self.clients_completed = 0
-        self.errored = 0
-        self.errorReason = None
         if opts.baseport:
             self.localport = opts.baseport
         else:
             self.localport = 0
 
-    def clientConnectionFailed(self, connector, reason):
-        self.clients_completed+=1
-        self.errored = 1
-        self.errorReason = reason
-        self.maybeDone()
-
-    def clientConnectionLost(self, connector, reason):
+    def clientDone(self):
         self.clients_completed+=1
         self.maybeDone()
 
     def maybeDone(self):
         if self.clients_created >= self.opts.count:
             if self.clients_completed == self.clients_created:
-                if self.errored:
-                    self.done.errback(reason)
-                else:
-                    print ("All clients done")
-                    self.done.callback(None)
+                print ("All clients done")
+                reactor.stop()
 
     def start_a_batch(self):
         self.clients_created += self.opts.batch
         for i in range(self.opts.batch):
-            reactor.connectTCP(self.opts.serverip, self.opts.serverport, self, bindAddress=(self.opts.localip,self.localport))
+            protocol = EchoClientDatagramProtocol(self, next(self.counter))
+            t = reactor.listenUDP(self.localport, protocol)
             if self.localport:
                 self.localport+=1
         if self.clients_created < self.opts.count:
@@ -134,11 +103,12 @@ def parse_options():
         cmd_options.batch = cmd_options.count
     return cmd_options
 
-def main(reactor):
-    opts= parse_options()
-    factory = EchoClientFactory(opts)
-    factory.start_a_batch()
-    return factory.done
+def main():
+    opts = parse_options()
+    mgr = Manager(opts)
+    mgr.start_a_batch()
+    reactor.run()
 
 if __name__ == '__main__':
-    task.react(main)
+    main()
+
