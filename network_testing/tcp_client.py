b6b7f304d40bbc5db7eb2de85aec070e3e0b2e45
lakshman kumar <lakshmankumar@gmail.com>
2019-03-05 13:58:28 -0800

    Implement timeout and base-port

--- network_testing/tcp_client.py
+++ network_testing/tcp_client.py
@@ -7,6 +7,7 @@
 from __future__ import print_function
 
 from twisted.internet import task
+from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineReceiver
@@ -14,9 +15,10 @@ from twisted.protocols.basic import LineReceiver
 import argparse
 import sys
 
+def echoClientCallback(arg):
+    arg.initiate_a_transaction()
 
 class EchoClient(LineReceiver):
-    end = b"Bye-bye!"
 
     def __init__(self):
         LineReceiver.__init__(self)
@@ -27,19 +29,23 @@ class EchoClient(LineReceiver):
         self.myinstance=next(self.factory.counter)
         self.inited=1
         self.factory.clients_created += 1
+        self.transactions_done = 0
 
     def connectionMade(self):
         if not self.inited:
             self.postFactorySetInit()
-        for i in range(self.factory.opts.transactions):
-            tosend = "This is line:{} from instance:{}".format(i, self.myinstance)
-            self.sendLine(bytes(tosend, 'utf-8'))
-        self.sendLine(self.end)
+        self.initiate_a_transaction()
 
+    def initiate_a_transaction(self):
+        tosend = "This is line:{} from instance:{}".format(self.transactions_done, self.myinstance)
+        self.sendLine(bytes(tosend, 'utf-8'))
 
     def lineReceived(self, line):
         print("receive:", line)
-        if line == self.end:
+        self.transactions_done += 1
+        if self.transactions_done < self.factory.opts.transactions:
+            reactor.callLater(self.factory.opts.timeout, echoClientCallback, self)
+        else:
             self.complete = 1
             self.transport.loseConnection()
 
@@ -94,11 +100,13 @@ class EchoClientFactory(ClientFactory):
 
 def parse_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l","--localip",   help="localip")
-    parser.add_argument("-s","--serverip",  help="serverip")
+    parser.add_argument("-l","--localip",    help="localip")
+    parser.add_argument("-b","--baseport",   help="use a baseport", type=int)
+    parser.add_argument("-s","--serverip",   help="serverip")
     parser.add_argument("-p","--serverport", help="serverport", type=int)
     parser.add_argument("-c","--count",      help="noofclients", type=int, default=1)
-    parser.add_argument("-t","--transactions", help="no of transactions for each client", type=int, default=3)
+    parser.add_argument("-n","--transactions", help="no of transactions for each client", type=int, default=3)
+    parser.add_argument("-t","--timeout",    help="timeout between each client in seconds", type=float, default=0)
     cmd_options = parser.parse_args()
     if not cmd_options.localip or not cmd_options.serverport or not cmd_options.serverip:
         print ("you should supply local-ip, serverip and server-port")
@@ -109,11 +117,14 @@ def parse_options():
 def main(reactor):
     opts= parse_options()
     factory = EchoClientFactory(opts)
+    port = 0
+    if opts.baseport:
+        port = opts.baseport
     for i in range(opts.count):
-        reactor.connectTCP(opts.serverip, opts.serverport, factory, bindAddress=(opts.localip,0))
+        reactor.connectTCP(opts.serverip, opts.serverport, factory, bindAddress=(opts.localip,port))
+        if opts.baseport:
+            port+=1
     return factory.done
-
-
 
 if __name__ == '__main__':
     task.react(main)
