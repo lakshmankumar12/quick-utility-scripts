391a8b237292352a2d57e87a5cb133d872771cf8
lakshman kumar <lakshmankumar@gmail.com>
2019-05-20 10:15:00 -0700

    verbose log for alert-listener

--- alert_listener.py
+++ alert_listener.py
@@ -19,7 +19,7 @@ alert_program= os.path.join(home,"github","mac_scripts","pop_up_dialog.sh")
 print ("Using alert - program: {}".format(alert_program))
 
 #invocation will throw error and halt script if there is a problem
-subprocess.check_output([alert_program,"Test Alert"])
+subprocess.check_output([alert_program,"Test Alert From Udp, Port:%s"%UDP_PORT])
 
 
 def main():
