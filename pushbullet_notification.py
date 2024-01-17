3c3ba75c61ca00dc9082a2a8866e30f6c081b17e
lakshman kumar <lakshmankumar@gmail.com>
2024-01-17 09:31:32 +0530

    pushbullet corrections

--- pushbullet_notification.py
+++ pushbullet_notification.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 if len(sys.argv) != 2:
-  print "Usage %s <message>"%sys.argv[0]
+  print ("Usage %s <message>"%sys.argv[0])
   sys.exit(1)
 
 with open (os.path.join(os.environ['HOME'], '.push_bullet_key')) as fd:
@@ -13,10 +13,10 @@ with open (os.path.join(os.environ['HOME'], '.push_bullet_key')) as fd:
 
 pb = Pushbullet(api_key)
 
-mobile_string='Samsung SM-G930T'
+mobile_string='Xiaomi 2201116TI'
 try:
   mobile = [ i for i in pb.devices if mobile_string in str(i) ][0]
 except IndexError:
-  print "Huh: couldn't find mobile_string(%s) in %s"%(mobile_string, pb.devices)
+  print ("Huh: couldn't find mobile_string(%s) in %s"%(mobile_string, pb.devices))
   sys.exit(1)
-mobile.push_note("Mac-Notification", sys.argv[1])
+mobile.push_note("Dev-Machine-Notification", sys.argv[1])
