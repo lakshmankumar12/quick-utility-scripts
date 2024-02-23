51ab3f79d24c2dc89d576c84b94c875a2431301b
lakshman kumar <lakshmankumar@gmail.com>
2024-02-23 16:01:17 +0530

    sctp client fine tuned

--- network_testing/simple_sctp_client.py
+++ network_testing/simple_sctp_client.py
@@ -1,37 +1,46 @@
+import argparse
 import socket
 import sctp
 
 
 ### Real simply client
 
-def sctp_init():
-    import socket
-    import sctp
+def sctp_init(opts):
 
-    serveraddr          = ("192.168.85.228", 38412)
+    serveraddr          = (opts.serverip, opts.serverport)
+    print ("Connecting to server:%s:%s"%serveraddr)
     sk = sctp.sctpsocket_tcp(socket.AF_INET)
     sk.connect(serveraddr)
 
+    print ("Connected")
 
-def full_conn():
-    size                = 1400
+    return sk
+
+
+def full_conn(sk):
+    size                = 1300
     msgFromClient       = "1234567890"*(size//10)
-    serveraddr          = ("10.0.3.253", 15151)
     recvbuffsize        = 10000
 
-    for i in range(50):
+    for i in range(100):
         print ("Trying size:%d"%(size+4*i))
         msgc = msgFromClient + "ABCD"*i
         bytesToSend  = str.encode(msgc)
 
-        sk = sctp.sctpsocket_tcp(socket.AF_INET)
-        sk.connect(serveraddr)
-
         sk.sctp_send(bytesToSend)
         print("Data-sent")
         data = sk.recv(10000).decode('utf-8')
-        print ("Got: %s"%data)
-        sk.shutdown(0)
-        sk.close()
+        print ("Server Reply: %s"%data)
+    sk.shutdown(0)
+    sk.close()
 
-full_conn()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("serverip", help="server ip")
+    parser.add_argument("serverport", help="server port", type=int, nargs="?", default=15151)
+    cmd_options = parser.parse_args()
+    return cmd_options
+
+opts = parse_args()
+sk = sctp_init(opts)
+full_conn(sk)
--- network_testing/simple_sctp_server.py
+++ network_testing/simple_sctp_server.py
@@ -1,34 +1,49 @@
+import argparse
 import socket
 import sctp
 
-host = '10.0.3.253'
+host = '10.0.2.1'
 port = 15151
 
-sock = sctp.sctpsocket_tcp(socket.AF_INET)
-sock.bind((host, port))
-sock.listen(1)
+def start_server(opts):
 
-while True:
-    # wait for a connection
-    print ('waiting for a connection')
-    connection, client_address = sock.accept()
+    print ("Waiting on ip:%s, port:%s"%(opts.myip, opts.myport))
 
-    try:
-        # show who connected to us
-        print ('connection from', client_address)
-        print (connection)
-        # receive the data in small chunks and print it
-        while True:
-            data = connection.recv(5000).decode('utf-8')
-            if data:
-                # output received data
-                print ("Data: %s" % data)
-                tosend = "We recieved " + str(len(data)) + " bytes from you"
-                connection.sendall(tosend.encode('utf-8'))
-            else:
-                # no more data -- quit the loop
-                print ("no more data.")
-                break
-    finally:
-        # Clean up the connection
-        connection.close()
+    sock = sctp.sctpsocket_tcp(socket.AF_INET)
+    sock.bind((opts.myip, opts.myport))
+    sock.listen(1)
+
+    while True:
+        # wait for a connection
+        print ('waiting for a connection')
+        connection, client_address = sock.accept()
+
+        try:
+            # show who connected to us
+            print ('connection from', client_address)
+            print (connection)
+            # receive the data in small chunks and print it
+            while True:
+                data = connection.recv(5000).decode('utf-8')
+                if data:
+                    # output received data
+                    print ("Data: %s" % data)
+                    tosend = "Received " + str(len(data)) + " bytes"
+                    connection.sendall(tosend.encode('utf-8'))
+                else:
+                    # no more data -- quit the loop
+                    print ("no more data.")
+                    break
+        finally:
+            # Clean up the connection
+            connection.close()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("myip", help="listen ip")
+    parser.add_argument("myport", help="listen port", type=int, nargs="?", default=15151)
+    cmd_options = parser.parse_args()
+    return cmd_options
+
+opts = parse_args()
+start_server(opts)
