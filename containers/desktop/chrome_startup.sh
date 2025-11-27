dae824fa3b07be2187ee063d265f1789dc9d9fb7
lakshman kumar <lakshmankumar@gmail.com>
2025-11-27 11:04:40 +0530

    compose changes for desktop/chrome only

--- containers/desktop/chrome_startup.sh
+++ containers/desktop/chrome_startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# 默认参数
 NOVNC_PORT="${PORT:-6080}"
 NOVNC_PASSWORD="${PASSWORD:-novncpass}"
 SSL_ENABLE="${SSL_ENABLE:-false}"
@@ -23,13 +22,12 @@ else
     echo "Unsupported arch: $ARCH"
     exit 1
 fi
-# 替换 supervisord.conf 中 chrome 的 command 行
+
 # with proxy
 sed -i "s|command=.*chrome.*|command=$CHROME_CMD --no-sandbox --disable-infobars --disable-crash-reporter --disable-fre --no-first-run --disable-default-apps --no-default-browser-check --disable-gpu --restore-last-session --remote-debugging-port=9222 --user-data-dir=/config --disable-dev-shm-usage --display=:99 --window-size=1280,1024 --proxy-server=\"socks5://tor-proxy:9050\" about:blank|" /etc/supervisor/supervisord.conf
 # without proxy
 #sed -i "s|command=.*chrome.*|command=$CHROME_CMD --no-sandbox --disable-infobars --disable-crash-reporter --disable-fre --no-first-run --disable-default-apps --no-default-browser-check --disable-gpu --restore-last-session --remote-debugging-port=9222 --user-data-dir=/config --disable-dev-shm-usage --display=:99 --window-size=1280,1024 about:blank|" /etc/supervisor/supervisord.conf
 
-# 修改 supervisord.conf中novnc命令根据是否启用SSL
 if [ "$SSL_ENABLE" = "true" ] && [ -f "$SSL_CERT_PATH" ] && [ -f "$SSL_KEY_PATH" ]; then
     echo "Starting noVNC with SSL on port $NOVNC_PORT"
     sed -i "s|command=.*novnc.*|command=/usr/bin/websockify --web /usr/share/novnc/ --cert=$SSL_CERT_PATH --key=$SSL_KEY_PATH --ssl-only $NOVNC_PORT localhost:5900|" /etc/supervisor/supervisord.conf
